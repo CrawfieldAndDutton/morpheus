d1e7e4801d8e42a8b7fc3c22e2b80f908d94688f
PaulAyushmaan <ayushmaanpaul2004@gmail.com>
2025-03-13 02:01:25 +0530

    made changes

--- src/apis/config.ts
+++ src/apis/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  baseUrl: "https://f565-2409-40e0-fd-fa74-9439-3873-5777-f0ff.ngrok-free.app/",
-  //  baseUrl: "https://127.0.0.1:8000",
+  // baseUrl: "https://f565-2409-40e0-fd-fa74-9439-3873-5777-f0ff.ngrok-free.app/",
+  baseUrl: "https://2d1f-2405-201-8004-90a0-5480-92e5-dcd8-3eb6.ngrok-free.app/",
 };
 
 export default config;
